d9e70025fa9ace35b072298805193960248c8df8
Kalp Kumar Varshney <kalpvars2@gmail.com>
2020-12-14 08:47:45 +0530

    Integrated redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import {searchRobots, requestRobots} from './reducers';
 import 'tachyons';
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
+  	<Provider store={store}>
 	  <App />
+	</Provider>
+  	}
   </React.StrictMode>,
   document.getElementById('root')
 );
